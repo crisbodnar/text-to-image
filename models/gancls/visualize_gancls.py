f24a756679a2460133b862f38684423d6b8b73e4
Cristian Bodnar <crisbodnar96@gmail.com>
2018-03-19 13:46:42 +0000

    Prepare visualisation modules for presentation

--- models/gancls/visualize_gancls.py
+++ models/gancls/visualize_gancls.py
@@ -16,8 +16,8 @@ class GanClsVisualizer(object):
         self.samples_dir = self.config.SAMPLE_DIR
 
     def visualize(self):
-        z = tf.placeholder(tf.float32, [self.model.batch_size, self.model.z_dim], name='z')
-        cond = tf.placeholder(tf.float32, [self.model.batch_size] + [self.model.embed_dim], name='cond')
+        z = tf.placeholder(tf.float32, [None, self.model.z_dim], name='z')
+        cond = tf.placeholder(tf.float32, [None] + [self.model.embed_dim], name='cond')
         gen = self.model.generator(z, cond, is_training=False)
 
         saver = tf.train.Saver(tf.global_variables('g_net'))
@@ -28,35 +28,47 @@ class GanClsVisualizer(object):
             print(" [!] Load failed...")
             raise LookupError('Could not load any checkpoints')
 
-        # Interpolation in z space:
-        # ---------------------------------------------------------------------------------------------------------
-        _, _, cond, _, _ = self.dataset.test.next_batch(1, window=4, embeddings=True)
-        cond = np.tile(cond, reps=[64, 1])
+        dataset_pos = None
+        for idx in range(6):
+            dataset_pos = np.random.randint(0, self.dataset.test.num_examples)
 
-        samples = gen_noise_interp_img(self.sess, gen, cond, self.model.z_dim, self.model.batch_size)
-        save_images(samples, get_balanced_factorization(samples.shape[0]),
-                    '{}/{}_visual/test1.png'.format(self.samples_dir, self.dataset.name))
+            # Interpolation in z space:
+            # ---------------------------------------------------------------------------------------------------------
+            _, cond, _, captions = self.dataset.test.next_batch_test(1, dataset_pos, 1)
+            cond = np.squeeze(cond, axis=0)
+            caption = captions[0][0]
 
-        # Interpolation in embedding space:
-        # ---------------------------------------------------------------------------------------------------------
+            samples = gen_noise_interp_img(self.sess, gen, cond, self.model.z_dim, self.model.batch_size)
+            save_cap_batch(samples, caption, '{}/{}_visual/z_interp/z_interp{}.png'.format(self.samples_dir,
+                                                                                           self.dataset.name,
+                                                                                           idx))
+            # Interpolation in embedding space:
+            # ---------------------------------------------------------------------------------------------------------
 
-        _, cond, _, _ = self.dataset.test.next_batch_test(2, 0, 1)
-        cond = np.squeeze(cond, axis=0)
-        cond1, cond2 = cond[0], cond[1]
+            _, cond, _, caps = self.dataset.test.next_batch_test(2, dataset_pos, 1)
+            cond = np.squeeze(cond, axis=0)
+            cond1, cond2 = cond[0], cond[1]
+            cap1, cap2 = caps[0][0], caps[1][0]
 
-        samples = gen_cond_interp_img(self.sess, gen, cond1, cond2, self.model.z_dim, self.model.batch_size)
-        save_images(samples, get_balanced_factorization(samples.shape[0]),
-                    '{}/{}_visual/test2.png'.format(self.samples_dir, self.dataset.name))
-        make_gif(samples, '{}/{}_visual/test2.gif'.format(self.samples_dir, self.dataset.name))
+            samples = gen_cond_interp_img(self.sess, gen, cond1, cond2, self.model.z_dim,
+                                          self.model.batch_size)
+            save_interp_cap_batch(samples, cap1, cap2,
+                                  '{}/{}_visual/cond_interp/cond_interp{}.png'.format(self.samples_dir,
+                                                                                      self.dataset.name,
+                                                                                      idx))
+            make_gif(samples, '{}/{}_visual/cond_interp/gifs/cond_interp{}.gif'.format(self.samples_dir,
+                                                                                       self.dataset.name,
+                                                                                       idx), duration=4)
 
-        # Generate captioned image
-        # ---------------------------------------------------------------------------------------------------------
-        _, conditions, _, captions = self.dataset.test.next_batch_test(2, 0, 1)
-        conditions = np.squeeze(conditions, axis=0)
-        caption = captions[0][0]
-        samples = gen_captioned_img(self.sess, gen, conditions[0], self.model.z_dim, self.model.batch_size)
+            # Generate captioned image
+            # ---------------------------------------------------------------------------------------------------------
+            _, conditions, _, captions = self.dataset.test.next_batch_test(1, dataset_pos, 1)
+            conditions = np.squeeze(conditions, axis=0)
+            caption = captions[0][0]
+            samples = gen_captioned_img(self.sess, gen, conditions, self.model.z_dim, self.model.batch_size)
 
-        save_cap_batch(samples, caption, '{}/{}_visual/caption.png'.format(self.samples_dir, self.dataset.name))
+            save_cap_batch(samples, caption, '{}/{}_visual/cap/cap{}.png'.format(self.samples_dir,
+                                                                                 self.dataset.name, idx))
 
 
 
